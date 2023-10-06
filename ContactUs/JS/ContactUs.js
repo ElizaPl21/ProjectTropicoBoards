23126a152ae953fa80866a6bda36c7aa1c61525f
dpechsima <dpechsima@gmail.com>
2023-10-05 23:39:39 -0600

    Cambios minimos realizados para la resolucion de conflictos en rama desarrollo

--- ContactUs/JS/ContactUs.js
+++ ContactUs/JS/ContactUs.js
@@ -4,6 +4,6 @@ const form = document.querySelector(".form");
 form.addEventListener('submit', manejoSubmit);
 
 //
-function manejoSubmit (evento){
+function manejoSubmit (event){
     evento.preventDefault()
 }
