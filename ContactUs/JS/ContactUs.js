da4011a668880ed6248ff62f65c89384ca0fb961
Daniel Pech <142923045+dpechsima@users.noreply.github.com>
2023-10-05 22:34:08 -0600

    Delete ContactUs directory

--- ContactUs/JS/ContactUs.js
+++
@@ -1,9 +0,0 @@
-//Selector para escuchar el evento 
-const form = document.querySelector(".form");
-
-form.addEventListener('submit', manejoSubmit);
-
-//
-function manejoSubmit (evento){
-    evento.preventDefault()
-}
