0d0e92879bd86db7e056ceb1829b5c7d51218051
dpechsima <dpechsima@gmail.com>
2023-10-11 14:10:47 -0600

    Se realizaon cmaios en la navbar hoja de estilos para resolver problemas de responsividad en Iphone SE, Se incluyen capetas de trabajo para la pagina de inicio

--- Login/js/Login.js
+++
@@ -1,9 +0,0 @@
-//Selector para escuchar el evento 
-const form = document.querySelector(".form");
-
-form.addEventListener('button', manejoSubmit);
-
-//
-function manejoSubmit (evento){
-    evento.preventDefault()
-}
