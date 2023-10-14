52b17879ad6d04ffac773ada22b89c49059a2ebd
ElizaPl21 <elipila21@gmail.com>
2023-10-14 17:43:25 -0600

    Modificación de JS y CSS

--- Productos/Js/productos.js
+++ Productos/Js/productos.js
@@ -2,40 +2,49 @@
 const division = document.querySelector(".list-group.list-group-flush");
 
 // Función para cargar productos desde el archivo JSON
-function fetchProductos() {
-  return fetch("productos.json")
-    .then((response) => response.json());
+const fetchProductos= async() => {
+  try{
+    const productos = await fetch("productos.json"); 
+  const parsedProductos= await productos.json(); 
+    return parsedProductos;
+} catch (err){
+    console.log(err);
+  }
 }
 
-// Función para mostrar un producto
+// Función para mostrar un producto en la DOM-Creando tarjeta
 function mostrarProducto(producto) {
   const card = document.createElement("div");
   card.style.setProperty("width", "18rem", "card-container");
   card.classList.add("card");
 
+ //Obteniendo imagen
   const image = document.createElement("img");
   image.classList.add("card-img-top", "imagencss");
   image.alt = "...";
   image.src = producto.imagen;
-  //image.style.width = "240px";
-  //image.style.height = "150px";
-
+ 
+//Obteniendo id
   const number = document.createElement("li");
   number.classList.add("list-group-item");
   number.textContent = `ID: ${producto.id}`;
 
+//Obteniendo nombre
   const name = document.createElement("li");
   name.classList.add("list-group-item");
   name.textContent = `Nombre: ${producto.productName}`;
 
+//Obteniendo precio
   const price = document.createElement("li");
   price.classList.add("list-group-item");
   price.textContent = `Precio: $${producto.productPrice}`;
 
+//Obteniendo descripción
   const description = document.createElement("li");
   description.classList.add("list-group-item");
   description.textContent = `Descripción: ${producto.productDescription}`;
 
+//Obteniendo stock
   const stock = document.createElement("li");
   stock.classList.add("list-group-item");
   stock.textContent = `En stock: ${producto.stockQuantity}`;
