c2ea53b2cd963416709990498834f8157f15ddcb
Irving1345 <irvin.roga@gmail.com>
2023-10-31 17:42:04 -0600

    se modifica pagina de nuevo carrito para que agrege los productos

--- NuevoCarrito/js/ServiceCart.js
+++ NuevoCarrito/js/ServiceCart.js
@@ -1,57 +1,55 @@
-//Esto es como lo tuyo solamente que no me funciona, no se si me equivoque en algo
 const cuentaCarritoElement = document.getElementById("cuenta-carrito");
-function agregarAlCarrito(producto){
-  const memoria = JSON.parse(localStorage.getItem("patinetas"));
-  let cuenta=0;
-  if(!memoria) {
-    const nuevoProducto = getNuevoProductoParaMemoria(producto)
-    localStorage.setItem("patinetas", JSON.stringify([nuevoProducto]));
-    cuenta=1;
-  }else {
-    const indiceProducto = memoria.findIndex(patinetas => patinetas.id === producto.id)
-    const nuevaMemoria = memoria;
-    if(indiceProducto === -1){
-      nuevaMemoria.push(getNuevoProductoParaMemoria(producto));
-      cuenta = 1;
-    } else {
-      nuevaMemoria[indiceProducto].cantidad ++;
-      cuenta = nuevaMemoria[indiceProducto].cantidad;
-    }
-    localStorage.setItem("patinetas",JSON.stringify(nuevaMemoria));
+
+function agregarAlCarrito(producto) {
+  const memoria = JSON.parse(localStorage.getItem("patinetas")) || [];
+  const indiceProducto = memoria.findIndex(patinetas => patinetas.id === producto.id);
+
+  if (indiceProducto === -1) {
+    memoria.push(getNuevoProductoParaMemoria(producto));
+  } else {
+    memoria[indiceProducto].cantidad++;
   }
+
+  localStorage.setItem("patinetas", JSON.stringify(memoria));
   actualizarNumeroCarrito();
-  return cuenta;
+  return memoria[indiceProducto].cantidad;
 }
 
-function restarAlCarrito(producto){
+function restarAlCarrito(producto) {
   const memoria = JSON.parse(localStorage.getItem("patinetas"));
-  const indiceProducto = memoria.findIndex(patinetas => patinetas.id === producto.id)
-  if(memoria[indiceProducto].cantidad===1){
-    memoria.splice(indiceProducto,1);
-  }else{
-  memoria[indiceProducto].cantidad--;
-}
-  localStorage.setItem("patinetas",JSON.stringify(memoria));
+  const indiceProducto = memoria.findIndex(patinetas => patinetas.id === producto.id);
+
+  if (memoria[indiceProducto].cantidad === 1) {
+    memoria.splice(indiceProducto, 1);
+  } else {
+    memoria[indiceProducto].cantidad--;
+  }
+
+  localStorage.setItem("patinetas", JSON.stringify(memoria));
   actualizarNumeroCarrito();
 }
 
-function getNuevoProductoParaMemoria(producto){
+function getNuevoProductoParaMemoria(producto) {
   const nuevoProducto = producto;
   nuevoProducto.cantidad = 1;
   return nuevoProducto;
 }
 
 
-function actualizarNumeroCarrito(){
+function actualizarNumeroCarrito() {
   const memoria = JSON.parse(localStorage.getItem("patinetas"));
-  if( cuenta && cuenta.length > 0){
-   const cuenta = memoria.reduce((acum, current)=>acum+current.cantidad,0)
-    return cuentaCarritoElement.innerText = cuenta;
+  if (memoria && memoria.length > 0) {
+    const cuenta = memoria.reduce((acum, current) => acum + current.cantidad, 0);
+    cuentaCarritoElement.innerText = cuenta;
+  } else {
+    cuentaCarritoElement.innerText = 0;
   }
-  cuentaCarritoElement.innerText = 0;
 }
 
-actualizarNumeroCarrito();
+
+
+
+
 
 /*
 //Esta es otra función funciona solo el refresh
--- NuevoCarrito/js/carrito.js
+++ NuevoCarrito/js/carrito.js
@@ -9,34 +9,33 @@ const reiniciarCarritoElement = document.getElementById("vacio");
 function crearTarjetas() {
   contenedorTarjetas.innerHTML = "";
   const productos = JSON.parse(localStorage.getItem("patinetas"));
-  console.log(productos);
   if (productos && productos.length > 0) {
     productos.forEach((producto) => {
       const nuevoProducto = document.createElement("div");
       nuevoProducto.classList = "tarjeta-patineta";
       nuevoProducto.innerHTML = `
-      <img src = ${producto.imagen}>
-      <h3 class="subtitulo contenido"> ${producto.productName}</h3> 
+      <img src="${producto.imagen}">
+      <h3 class="subtitulo contenido">${producto.productName}</h3> 
       <p class="precio contenido">${producto.productPrice}</p>
       <div>
-          <button> - </button>
+          <button class="restar"> - </button>
           <span class="cantidad">${producto.cantidad}</span>
-          <button> + </button>
+          <button class="sumar"> + </button>
       </div>
   `;
       contenedorTarjetas.appendChild(nuevoProducto);
+
       nuevoProducto
-        .getElementsByTagName("button")[1]
-        .addEventListener("click", (e) => {
-          const cuentaElement = e.target.parentElement.getElementsByClassName("span")[0];
+        .getElementsByClassName("sumar")[0]
+        .addEventListener("click", () => {
+          const cuentaElement = nuevoProducto.getElementsByClassName("cantidad")[0];
           cuentaElement.innerText = agregarAlCarrito(producto);
-          crearTarjetasProductosCarrito();
           actualizarTotales();
         });
 
       nuevoProducto
-        .getElementsByTagName("button")[0]
-        .addEventListener("click", (e) => {
+        .getElementsByClassName("restar")[0]
+        .addEventListener("click", () => {
           restarAlCarrito(producto);
           crearTarjetas();
           actualizarTotales();
@@ -44,8 +43,39 @@ function crearTarjetas() {
     });
   }
 }
-crearTarjetas();
-actualizarTotales();
+
+function agregarAlCarrito(producto) {
+  const memoria = JSON.parse(localStorage.getItem("patinetas")) || [];
+  const indiceProducto = memoria.findIndex((patinetas) => patinetas.id === producto.id);
+
+  if (indiceProducto === -1) {
+    memoria.push(getNuevoProductoParaMemoria(producto));
+  } else {
+    memoria[indiceProducto].cantidad++;
+  }
+
+  localStorage.setItem("patinetas", JSON.stringify(memoria));
+  return memoria[indiceProducto].cantidad;
+}
+
+function restarAlCarrito(producto) {
+  const memoria = JSON.parse(localStorage.getItem("patinetas"));
+  const indiceProducto = memoria.findIndex((patinetas) => patinetas.id === producto.id);
+
+  if (memoria[indiceProducto].cantidad === 1) {
+    memoria.splice(indiceProducto, 1);
+  } else {
+    memoria[indiceProducto].cantidad--;
+  }
+
+  localStorage.setItem("patinetas", JSON.stringify(memoria));
+}
+
+function getNuevoProductoParaMemoria(producto) {
+  const nuevoProducto = producto;
+  nuevoProducto.cantidad = 1;
+  return nuevoProducto;
+}
 
 function actualizarTotales() {
   const productos = JSON.parse(localStorage.getItem("patinetas"));
@@ -54,30 +84,34 @@ function actualizarTotales() {
   if (productos && productos.length > 0) {
     productos.forEach((producto) => {
       unidades += producto.cantidad;
-      precio += producto.precio * producto.cantidad;
+      precio += producto.productPrice * producto.cantidad;
     });
+  }
   
   unidadesElement.innerText = unidades;
   precioElement.innerText = precio;
-}
-revisarMensajeVacio();
+  revisarMensajeVacio();
 }
 
 function revisarMensajeVacio() {
   const productos = JSON.parse(localStorage.getItem("patinetas"));
-  console.log(productos, productos == true);
   carritoVacioElement.classList.toggle("escondido", productos && productos.length > 0);
-  totalesContainer.classList.toggle("escondido", !(productos && productos.length > 0));
+  totalesElement.classList.toggle("escondido", !(productos && productos.length > 0));
 }
 
 revisarMensajeVacio();
 
 reiniciarCarritoElement.addEventListener("click", reiniciarCarrito);
+
 function reiniciarCarrito() {
   localStorage.removeItem("patinetas");
   actualizarTotales();
   crearTarjetas();
 }
+
+crearTarjetas();
+actualizarTotales();
+
 
 /*
 //Segunda manera conrefresh funciona con html en cantidad en vez de unidades
