f116f694b97a61ca3d209a807630229b9b177ddc
Irving1345 <142923421+Irving1345@users.noreply.github.com>
2023-10-31 11:38:37 -0600

    Delete Pago directory

--- Pago/pago.js
+++
@@ -1,9 +0,0 @@
-function validarFormulario() {
-    var form = document.querySelector("form.needs-validation");
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    form.classList.add("was-validated");
-    return form.checkValidity();
-  }
